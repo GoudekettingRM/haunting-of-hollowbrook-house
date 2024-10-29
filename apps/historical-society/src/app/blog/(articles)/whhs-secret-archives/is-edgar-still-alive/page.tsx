49d24d8827dfb7dd8f55953f3005fe9ec5cefc58
GoudekettingRM <r.goudeketting@gmail.com>
2024-10-29 11:38:48 +0100

    Add no follow no index to secret page

--- apps/historical-society/src/app/blog/(articles)/whhs-secret-archives/is-edgar-still-alive/page.tsx
+++ apps/historical-society/src/app/blog/(articles)/whhs-secret-archives/is-edgar-still-alive/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Secret Archives | Is Edgar Still Alive?',
+  description:
+    'Is Edgar Hollowbrook still alive? Evidence has surfaced that suggests the disappearance of the Hollowbrook patriarch may not have been as straightforward as it seemed.',
+  robots: 'noindex, nofollow',
+};
+
 const IsEdgarStillAlive = () => {
   return <div>IsEdgarStillAlive</div>;
 };

70284b85876b574945b9bf58fc06d80ad0b9228e
GoudekettingRM <r.goudeketting@gmail.com>
2024-10-29 10:08:46 +0100

    Fix styling for chat button

--- apps/historical-society/src/components/ChatWindow.tsx
+++ apps/historical-society/src/components/ChatWindow.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BellIcon, InfoIcon } from 'lucide-react';
+import { InfoIcon, MessagesSquare } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 
 const ChatWindow = () => {
@@ -185,12 +185,12 @@ const ChatWindow = () => {
             onClick={() => setIsChatOpen((prev) => !prev)}
             aria-label='Open messages'
             type='button'
-            className='mt-4 ml-auto block'
+            className={`mt-4 mr-2 ml-auto block p-3 rounded-full bg-parchment text-dark-wood hover:bg-opacity-100 ${isChatOpen ? 'bg-opacity-100' : 'bg-opacity-80'}`}
           >
-            <BellIcon size={32} />
+            <MessagesSquare size={32} />
           </button>
           {unreadMessages > 0 && (
-            <div className='absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center'>
+            <div className='absolute -top-1 right-1 bg-red-500 text-white text-xs rounded-full w-6 h-6 flex items-center justify-center'>
               {unreadMessages}
             </div>
           )}

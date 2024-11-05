cfb507620eb863f173c26ff8c38fe82da05cae50
GoudekettingRM <r.goudeketting@gmail.com>
2024-11-05 11:18:29 +0100

    Update type

--- apps/historical-society/src/app/api/sys-redirect/route.ts
+++ apps/historical-society/src/app/api/sys-redirect/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-    const body = await request.json();
+    const body: { search: string } = await request.json();
 
     if (body.search.toLowerCase().replace(/\s+/g, '') === 'edgarisstillalive') {
       return NextResponse.json({ redirectUrl: '/sys-admin/protocol-9139' }, { status: 200 });

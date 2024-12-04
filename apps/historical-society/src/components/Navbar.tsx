7e1958a9c9208a714fe296a0607211e474502bd0
GoudekettingRM <r.goudeketting@gmail.com>
2024-12-04 15:45:37 +0100

    Remove with letters stuff

--- apps/historical-society/src/components/Navbar.tsx
+++ apps/historical-society/src/components/Navbar.tsx
@@ -1,32 +1,26 @@
 'use client';
 
-import { WithDisappearingLetters } from '@repo/ui/with-disappearing-letters';
 import Link from 'next/link';
 import SearchBar from './SearchBar';
-import { useLetterDropTimer } from './useLetterDropTimer';
 
 function Navbar() {
-  const enabled = useLetterDropTimer(10000);
-
   return (
-    <WithDisappearingLetters config={{ enabled }}>
-      <nav className='flex flex-col p-4'>
-        <Link href='/' className='text-2xl' title='Whispering Hollows Historical Society'>
-          <span className='block'>
-            WHHS<span className='text-4xl'>.</span>
-          </span>
-          <span className='block text-sm font-serif italic'>Whispering Hollows Historical Society</span>
-        </Link>
+    <nav className='flex flex-col p-4'>
+      <Link href='/' className='text-2xl' title='Whispering Hollows Historical Society'>
+        <span className='block'>
+          WHHS<span className='text-4xl'>.</span>
+        </span>
+        <span className='block text-sm font-serif italic'>Whispering Hollows Historical Society</span>
+      </Link>
 
-        <div className='my-4'>
-          <SearchBar />
-        </div>
+      <div className='my-4'>
+        <SearchBar />
+      </div>
 
-        <NavLink name='Home' href='/' />
-        <NavLink name='Blog' href='/blog' />
-        <NavLink name='Contact' href='/contact' />
-      </nav>
-    </WithDisappearingLetters>
+      <NavLink name='Home' href='/' />
+      <NavLink name='Blog' href='/blog' />
+      <NavLink name='Contact' href='/contact' />
+    </nav>
   );
 }
 export default Navbar;
